0946ced6a3398b8ebac395e437ab9fe6d84c4516
Timur <emailtotimur@gmail.com>
2017-05-28 22:14:11 +0300

    .

--- LovelyWaffles.Web/Controllers/AdminController.cs
+++ LovelyWaffles.Web/Controllers/AdminController.cs
@@ -1,19 +1,59 @@
-﻿using System;
+﻿using LovelyWaffles.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace LovelyWaffles.Web.Controllers
 {
-    [Authorize]
     public class AdminController : Controller
     {
         //
         // GET: /Admin/
+        [Authorize]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            LoginViewModel model = new LoginViewModel();
+
+            return View(model);
+        }
+
+        // POST: /Account/Login
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (this.ModelState.IsValid && FormsAuthentication.Authenticate(model.UserName, model.Password))
+            {
+                FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                if (this.Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "admin");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            this.ModelState.AddModelError("", "Неверное имя пользователя или пароль.");
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("Index", "Home");
         }
 	}
 }
---
+++ LovelyWaffles.Web/Startup.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LovelyWaffles.Web
+{
+    public class Startup
+    {
+    }
+}
