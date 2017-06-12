cd2e1b802f6671609137700ea4d4d5895bdaee20
Timur <emailtotimur@gmail.com>
2017-06-12 17:59:30 +0300

    Add Error Handler

---
+++ LovelyWaffles.Web/Controllers/ErrorHandlerController.cs
@@ -0,0 +1,22 @@
+﻿using LovelyWaffles.Data.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LovelyWaffles.Web.Controllers
+{
+    public class ErrorHandlerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+	}
+}
